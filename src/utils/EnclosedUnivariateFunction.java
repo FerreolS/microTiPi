9bdaca2b9b39851b65f8a79e1f62bf31b071b0a2
ferreol <ferreol.soulez@epfl.ch>
2017-02-14 08:48:53 +0100

    Add EnclosedUnivariateFunction

---
+++ src/utils/EnclosedUnivariateFunction.java
@@ -0,0 +1,32 @@
+package utils;
+
+import org.apache.commons.math3.analysis.UnivariateFunction;
+
+import mitiv.base.mapping.DoubleFunction;
+import mitiv.base.mapping.FloatFunction;
+
+public class EnclosedUnivariateFunction implements DoubleFunction, FloatFunction {
+
+    UnivariateFunction func;
+    double scale;
+
+    public EnclosedUnivariateFunction(UnivariateFunction func, double scale){
+        this.func = func;
+        this.scale = scale;
+    }
+
+    public EnclosedUnivariateFunction(UnivariateFunction func){
+        this( func,1);
+    }
+
+    @Override
+    public float apply(float arg) {
+        return (float) func.value(scale * arg);
+    }
+
+    @Override
+    public double apply(double arg) {
+        return func.value(scale * arg);
+    }
+
+}
