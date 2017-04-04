f241d757d82374068db334292d251147b4d09ac7
ferreol <ferreol.soulez@epfl.ch>
2017-04-04 10:15:07 +0200

    Try to add scale

--- src/microTiPi/biphoton/BiPhotonModel.java
+++ src/microTiPi/biphoton/BiPhotonModel.java
@@ -3,6 +3,10 @@ package microTiPi.biphoton;
 import microTiPi.epifluorescence.WideFieldModel;
 import microTiPi.microscopy.MicroscopeModel;
 import mitiv.array.Array3D;
+import mitiv.array.Double3D;
+import mitiv.array.DoubleArray;
+import mitiv.array.Float3D;
+import mitiv.array.FloatArray;
 import mitiv.array.impl.FlatDouble3D;
 import mitiv.array.impl.FlatFloat3D;
 import mitiv.base.Shape;
@@ -28,6 +32,8 @@ public class BiPhotonModel extends MicroscopeModel {
     protected int nModulus;
     protected int nDefocus;
     protected int nPhase;
+
+    double scale =1.;
 
     /**
      * index of defocus parameter space
@@ -74,11 +80,8 @@ public class BiPhotonModel extends MicroscopeModel {
         super(psfShape, dxy, dz, single);
         wfPSF = new WideFieldModel(psfShape, nPhase, nModulus, NA, lambda, ni, dxy, dz, radial, single);
 
-
         parameterSpace = new DoubleShapedVectorSpace[3];
         parameterCoefs = new DoubleShapedVector[3];
-
-
 
         parameterSpace[DEFOCUS] =  wfPSF.getDefocusCoefs().getOwner();
         parameterCoefs[DEFOCUS] = wfPSF.getDefocusCoefs();
@@ -88,7 +91,15 @@ public class BiPhotonModel extends MicroscopeModel {
             nModulus = 1;
         }
         setNModulus(nModulus);
+        computePSF();
+        scale = 1./ ((DoubleArray) psf).sum();
+        if(single){
+            ((FloatArray) psf).scale( (float) scale);
+        }else{
+            ((DoubleArray) psf).scale( scale);
+        }
     }
+
     @Override
     public
     void computePSF() {
@@ -104,6 +115,8 @@ public class BiPhotonModel extends MicroscopeModel {
                     return arg*arg;
                 }
             });
+            ((Float3D) psf).scale((float) scale);
+            //    ((Float3D) psf).scale((float) (1./((Float3D) psf).sum()));
         }else{
             psf = wfPSF.getPSF().copy();
             psf.flatten();
@@ -113,9 +126,10 @@ public class BiPhotonModel extends MicroscopeModel {
                     return arg*arg;
                 }
             });
+            ((Double3D) psf).scale(scale);
+            //   ((Double3D) psf).scale(1./((Double3D) psf).sum());
         }
         PState = 1;
-
     }
 
     /* (non-Javadoc)
@@ -136,7 +150,6 @@ public class BiPhotonModel extends MicroscopeModel {
     public void setParam(DoubleShapedVector param) {
         wfPSF.setParam(param);
         PState = 0;
-
     }
 
     /* (non-Javadoc)
@@ -144,34 +157,16 @@ public class BiPhotonModel extends MicroscopeModel {
      */
     @Override
     public DoubleShapedVector apply_Jacobian(ShapedVector grad, ShapedVectorSpace xspace) {
-        // psf= h(param)^2
-        // dh /param  -> wfPSF.apply_Jacobian(grad, xspace);
-        // dpsf /dh = 2 h
-
         if(single){
-            FlatFloat3D psf2 = (FlatFloat3D) wfPSF.getPSF().copy();
-            psf2.map(new FloatFunction(){
-                @Override
-                public float apply(float arg) {
-                    return 2*arg;
-                }
-            });
+            Float3D psf2 = (Float3D) wfPSF.getPSF().copy();
+            psf2.scale((float) (2.*scale));
             ((FloatShapedVector) grad).multiply(grad.getSpace().create(psf2));
         }
         else{
-            FlatDouble3D psf2 = (FlatDouble3D) wfPSF.getPSF().copy();
-            psf2.map(new DoubleFunction(){
-                @Override
-                public double apply(double arg) {
-                    return 2*arg;
-                }
-            });
+            Double3D psf2 = (Double3D) wfPSF.getPSF().copy();
+            psf2.scale(2.*scale);
             ((DoubleShapedVector) grad).multiply(grad.getSpace().create(psf2));
-
-
         }
-
-        // TODO Auto-generated method stub
         return wfPSF.apply_Jacobian(grad, xspace);
     }
 
@@ -182,7 +177,6 @@ public class BiPhotonModel extends MicroscopeModel {
     public void freePSF() {
         PState =0;
         psf = null;
-
     }
 
     /**
@@ -314,6 +308,16 @@ public class BiPhotonModel extends MicroscopeModel {
      */
     public Double getNi() {
         return wfPSF.getNi();
+    }
+
+    public double getScale(){
+        return scale;
+    }
+    public void setScale(double scl){
+        scale = scl;
+    }
+    public void updateScale(double updt){
+        scale *= updt;
     }
 }
 
